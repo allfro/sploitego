b4503b839c994d835d030f58782ece5561275d9a
allfro <ndouba@gmail.com>
2012-08-02 18:55:31 -0400

    Just prettied it up a bit in the wizard generation stuff.

--- setup.py
+++ setup.py
@@ -34,21 +34,21 @@ setup(
         '' : [ '*.gif', '*.png', '*.conf', '*.plate' ]
     },
     install_requires=[
-        #'easygui>=0.94',
-        #'gnuplot-py>=1.8',
+#        'easygui>=0.94',
+#        'gnuplot-py>=1.8',
         'msgpack-python>=0.1.12',
-        #'numpy==1.5.1',
+#        'numpy==1.5.1',
         'pexpect>=2.4',
         'pycrypto>=2.1',
-        #'pylibpcap>=0.6.2',
+#        'pylibpcap>=0.6.2',
         'readline>=6.2.2',
-        #'scapy==2.1.0'
+#        'scapy==2.1.0'
     ],
     dependency_links=[
-        #'http://www.secdev.org/projects/scapy/files/',
-        #'http://sourceforge.net/projects/easygui/files/0.96/',
-        #'http://sourceforge.net/projects/pylibpcap/files/pylibpcap/0.6.4/',
-        #'http://libdnet.googlecode.com/files/'
+#        'http://www.secdev.org/projects/scapy/files/',
+#        'http://sourceforge.net/projects/easygui/files/0.96/',
+#        'http://sourceforge.net/projects/pylibpcap/files/pylibpcap/0.6.4/',
+#        'http://libdnet.googlecode.com/files/'
     ]
 )
 
