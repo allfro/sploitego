e5af5b0e9dc4da196ac469d30a9388dc2bf4b038
allfro <ndouba@gmail.com>
2013-03-19 19:55:35 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, os
 
+scripts = [
+    'src/scripts/qtmsfconsole'
+]
+
+if os.name == 'nt':
+    scripts += ['%s.bat' % s for s in scripts]
 
 setup(
     name='sploitego',
     author='Nadeem Douba',
-    version='1.2.2',
+    version='1.3',
     author_email='ndouba@gmail.com',
     description='Penetration testing transforms for Maltego.',
     license='GPL',
     packages=find_packages('src'),
     package_dir={ '' : 'src' },
-    scripts=[],
+    scripts=scripts,
     zip_safe=False,
     package_data={
         '' : [ '*.gif', '*.png', '*.conf', '*.mtz', '*.machine' ]
@@ -21,7 +27,9 @@ setup(
         'scapy==2.1.0',
         'pynessusrpc',
         'pyiptools',
-        'canari'
+        'canari',
+        'dnspython',
+        'lxml'
     ],
     dependency_links=[
         'http://www.secdev.org/projects/scapy/files/'
