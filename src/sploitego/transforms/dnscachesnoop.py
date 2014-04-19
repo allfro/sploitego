ca899a686de520ae09c016a5c8fdfc6b08b7e3ed
allfro <ndouba@gmail.com>
2014-04-19 00:03:36 -0400

    Using dnspython instead of scapy

--- src/sploitego/transforms/dnscachesnoop.py
+++ src/sploitego/transforms/dnscachesnoop.py
@@ -1,14 +1,18 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 from canari.maltego.entities import NSRecord, DNSName, IPv4Address
 from canari.maltego.message import Label, UIMessage
-from sploitego.scapytools.dns import nslookup
 from canari.framework import configure
 from canari.maltego.utils import debug
 from canari.maltego.html import Table
 from canari.config import config
-from scapy.all import DNS
 
+import dns.query
+import dns.message
+import dns.rdatatype
+import dns.rdataclass
+
+from common.dnstools import nslookup_raw
 
 __author__ = 'Nadeem Douba'
 __copyright__ = 'Copyright 2012, Sploitego Project'
@@ -33,32 +37,33 @@ __all__ = [
         'sploitego.v2.NSRecordToDNSName_CacheSnoop'
     ],
     inputs=[
-        ( 'Reconnaissance', IPv4Address ),
-        ( 'Reconnaissance', NSRecord )
+        ('Reconnaissance', IPv4Address),
+        ('Reconnaissance', NSRecord)
     ]
 )
 def dotransform(request, response):
-    ip = request.value
-    ans = nslookup("www.google.ca", nameserver=ip)
-    if ans is not None:
+    nameserver = request.value
+
+    if nslookup_raw('www.google.ca', resolver=nameserver).answer:
         for site in config['dnscachesnoop/wordlist']:
             debug('Resolving %s' % site)
-            ans = nslookup(site, nameserver=ip, rd=0)
-            if not ans[DNS].ancount:
-                ans = nslookup('www.%s' % site, nameserver=ip, rd=0)
-            if ans[DNS].ancount:
+
+            msg = nslookup_raw(site, resolver=nameserver, recursive=False)
+            if not msg.answer:
+                msg = nslookup_raw('www.%s' % site, resolver=nameserver, recursive=False)
+            if msg.answer:
                 e = DNSName(site)
                 t = Table(['Name', 'Query Class', 'Query Type', 'Data', 'TTL'], 'Cached Answers')
-                for i in range(0, ans[DNS].ancount):
-                    rr = ans[DNS].an[i]
-                    t.addrow([
-                            rr.rrname.rstrip('.'),
-                            rr.sprintf('%rclass%'),
-                            rr.sprintf('%type%'),
-                            rr.rdata.rstrip('.'),
-                            rr.sprintf('%ttl%')
+                for rrset in msg.answer:
+                    for rr in rrset:
+                        t.addrow([
+                            rrset.name.to_text(),
+                            dns.rdataclass.to_text(rr.rdclass),
+                            dns.rdatatype.to_text(rr.rdtype),
+                            rr.to_text(),
+                            rrset.ttl
                         ])
-                e += Label('Cached Answers', t, type='text/html')
+                e += Label('Cached Answers from %s' % nameserver, t, type='text/html')
                 response += e
     else:
         response += UIMessage('DNS server did not respond to initial DNS request.')
