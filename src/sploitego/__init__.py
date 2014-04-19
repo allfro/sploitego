780a71ae8d0481d4c0fd157f101a04058a5ffc42
allfro <ndouba@gmail.com>
2014-04-18 23:15:55 -0400

    Updated version number to coincide with package version.

--- src/sploitego/__init__.py
+++ src/sploitego/__init__.py
@@ -5,7 +5,7 @@ __copyright__ = 'Copyright 2012, Sploitego Project'
 __credits__ = []
 
 __license__ = 'GPL'
-__version__ = '0.1'
+__version__ = '1.5'
 __maintainer__ = 'Nadeem Douba'
 __email__ = 'ndouba@gmail.com'
 __status__ = 'Development'
